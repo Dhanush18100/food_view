658b40a28c933406be2f27a90245e970f5812cc1
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-12-12 08:18:56 +0530

    commit

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -33,36 +33,42 @@ async function likeFood(req, res) {
     const { foodId } = req.body;
     const user = req.user;
 
-    const isAlreadyLiked = await likeModel.findOne({ user: user._id, food: foodId });
+    const isAlreadyLiked = await likeModel.findOne({
+        user: user._id,
+        food: foodId
+    })
 
     if (isAlreadyLiked) {
-        await likeModel.deleteOne({ user: user._id, food: foodId });
+        await likeModel.deleteOne({
+            user: user._id,
+            food: foodId
+        })
 
-        const updated = await foodModel.findByIdAndUpdate(
-            foodId,
-            { $inc: { likeCount: -1 } },
-            { new: true }
-        );
+        await foodModel.findByIdAndUpdate(foodId, {
+            $inc: { likeCount: -1 }
+        })
 
         return res.status(200).json({
-            liked: false,
-            likeCount: updated.likeCount
-        });
+            message: "Food unliked successfully"
+        })
     }
 
-    await likeModel.create({ user: user._id, food: foodId });
+    const like = await likeModel.create({
+        user: user._id,
+        food: foodId
+    })
 
-    const updated = await foodModel.findByIdAndUpdate(
-        foodId,
-        { $inc: { likeCount: 1 } },
-        { new: true }
-    );
+    await foodModel.findByIdAndUpdate(foodId, {
+        $inc: { likeCount: 1 }
+    })
 
     res.status(201).json({
-        liked: true,
-        likeCount: updated.likeCount
-    });
+        message: "Food liked successfully",
+        like
+    })
+
 }
+
 
 
 async function saveFood(req, res) {
